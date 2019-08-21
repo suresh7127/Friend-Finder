0e12da7e8bc068a49c4d3e2c3eb120ce1688a429
suresh <sureshmahato@Sureshs-MacBook-Pro.local>
2019-08-21 18:53:22 -0500

    Application's data created with an array of objects

---
+++ app/data/friends.js
@@ -0,0 +1,70 @@
+let friends = [
+  {
+    "name": "Barak Obama",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/BarackObama2005portrait_edit2.jpg/1200px-BarackObama2005portrait_edit2.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      3
+    ]
+  },
+  {
+    "name": "Steven Seagal",
+    "photo": "http://cdn-static.denofgeek.com/sites/denofgeek/files/styles/main_wide/public/2017/02/header_1.jpg?itok=bqQ00vcZ",
+    "scores": [
+      3,
+      2,
+      5,
+      1,
+      2
+    ]
+  },
+  {
+    "name": "Jean-Claude Van Damme",
+    "photo": "https://www.thefamouspeople.com/profiles/images/jean-claude-van-damme-3.jpg",
+    "scores": [
+      2,
+      3,
+      4,
+      4,
+      3
+    ]
+  },
+  {
+    "name": "Bruce Willis",
+    "photo": "https://timedotcom.files.wordpress.com/2015/03/bruce-willis-movies-01.jpg?quality=85&w=838",
+    "scores": [
+      5,
+      5,
+      5,
+      5,
+      4
+    ]
+  },
+  {
+    "name": "Clint Eastwood",
+    "photo": "https://www.thesun.co.uk/wp-content/uploads/2017/06/nintchdbpict000190860133.jpg?strip=all&w=960",
+    "scores": [
+      2,
+      3,
+      4,
+      5,
+      1
+    ]
+  },
+  {
+    "name": "Dread Pirate Roberts",
+    "photo": "http://www.slate.com/content/dam/slate/blogs/moneybox/2013/11/06/silk_road_2_0_dread_pirate_roberts_is_resurrected/1383765045.png.CROP.promo-mediumlarge.png",
+    "scores": [
+      4,
+      2,
+      3,
+      1,
+      4
+    ]
+  }
+];
+
+module.exports = friends;
