7403c5f1e9abb8e2bb13311ee81afebe31bc14cb
suresh <sureshmahato@Sureshs-MacBook-Pro.local>
2019-08-23 13:20:19 -0500

    On click event created for user for submit button

---
+++ logic.js
@@ -0,0 +1,42 @@
+let newUser = {};
+let bestFriend = {
+  diff: 100,
+  name: "",
+  image: ""
+};
+
+
+$('#submit').on('click', () => {
+  let userName = $('#name').val().trim();
+  let imag = $('#image').val().trim();
+  let ans1 = $('#quest1').val().trim();
+  let ans2 = $('#quest2').val().trim();
+  let ans3 = $('#quest3').val().trim();
+  let ans4 = $('#quest4').val().trim();
+  let ans5 = $('#quest5').val().trim();
+
+  if (userName === "" || imag === "" || ans1 === "" || ans2 === "" || ans3 === "" || ans4 === "" || ans5 === "") {
+    alert("Please fill in all fields");
+  } else {
+    newUser = {
+      name: userName,
+      photo: imag,
+      scores: [ans1, ans2, ans3, ans4, ans5]
+    };
+    findFriend(newUser.scores);
+
+    setTimeout(postData, 1500);
+
+    function postData() {
+      $.post({ url: '/api/friends', contentType: 'application/json' }, JSON.stringify(newUser));
+    }
+
+    $('#name').val("");
+    $('#image').val("");
+    $('#quest1').val("");
+    $('#quest2').val("");
+    $('#quest3').val("");
+    $('#quest4').val("");
+    $('#quest5').val("");
+  }
+});
