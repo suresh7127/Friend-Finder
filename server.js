47b5285c19e0b366a3d72f79b36211d27f6d362d
suresh <sureshmahato@Sureshs-MacBook-Pro.local>
2019-08-24 08:23:36 -0500

    Static CSS service added

--- server.js
+++ server.js
@@ -6,3 +6,10 @@ var path = require("path");
 var app = express();
 
 var PORT = process.env.PORT || 8080;
+// For serving of static CSS
+app.use(express.static(__dirname + "/app/css"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
